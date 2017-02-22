c180eeb17da8ea44e8b1502a465f307a8c5e0ebb
Matt Fuller <matthewdf10@gmail.com>
2017-02-22 11:28:50 -0500

    disabling env variable encryption for aqua

--- v1/opt/aqua/util/aqua-api.sh
+++ v1/opt/aqua/util/aqua-api.sh
@@ -16,7 +16,7 @@ done
 
 sudo curl -H "Content-Type: application/json" -u administrator:$SCALOCK_ADMIN_PASSWORD -X POST -d '{"name":"core-user rule","description": "Core User is Admin of all containers","role":"administrator","resources":{"containers":["*"],"images":["*"],"volumes":["*"],"networks":["*"]},"accessors":{"users":["core"]}}' http://localhost:8083/api/v1/adminrules
 
-sudo curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"name": "Ethos", "type": "security.profile", "description": "Ethos Default RunTime Profile", "encrypt_all_envs": true}' -u administrator:$SCALOCK_ADMIN_PASSWORD http://localhost:8083/api/v1/securityprofiles
+sudo curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"name": "Ethos", "type": "security.profile", "description": "Ethos Default RunTime Profile", "encrypt_all_envs": false}' -u administrator:$SCALOCK_ADMIN_PASSWORD http://localhost:8083/api/v1/securityprofiles
 
 CRED_DIR="/opt/aqua"
 if [[ ! -d $CRED_DIR ]]; then
