d032085340a573cc4142d2395de25209f04618fb
Matt Fuller <fuller@adobe.com>
2017-03-09 17:47:18 -0500

    updating aqua to 2.0.2 (#84)

--- v1/opt/aqua/setup/leader/001-images.sh
+++ v1/opt/aqua/setup/leader/001-images.sh
@@ -4,6 +4,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $DIR/../../../../lib/helpers.sh
 
 etcd-set /images/scalock-gateway    "index.docker.io/aquasec/gateway:2.0"
-etcd-set /images/scalock-agent      "index.docker.io/aquasec/agent:2.0.eof-fix"
+etcd-set /images/scalock-agent      "index.docker.io/aquasec/agent:2.0.2"
 etcd-set /images/scalock-server     "index.docker.io/aquasec/server:2.0"
 
